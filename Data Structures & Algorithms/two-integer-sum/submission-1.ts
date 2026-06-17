28b37ae45d865d2d832c81024e2a4430774d74d0
Luis Buenrostro <hello@luisbuenrostro.dev>
2026-06-17 08:56:35 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        // Naive approach is to loop through rest of elems for each indice
+        // Time complexity: O(n^2) Quadratic, Space Complexity: O(1)
+
+        // Optimized approach we store each val in a HashMap where we check
+        // if the current number (indice) is the complimentary of the value (target - val)
+        // if true return both indices as answer { complimentary : indice }
+
+        var complimentary = 0;
+        const complMap = new Map();
+        const ans = [0, 0];
+
+        for (let i = 0; i < nums.length; i++) {
+            if (complMap.has(nums[i])) {
+                // Answer is found, build out array
+                ans[0] = i;
+                ans[1] = complMap.get(nums[i])
+            } else {
+            complimentary = target - nums[i];
+            complMap.set(complimentary, i)
+            }
+        }
+
+
+
+        return ans;
+    }
+}
