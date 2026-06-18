83c4aa27c4a9f8e53c08a6609e49b2fc0132b795
Luis Buenrostro <hello@luisbuenrostro.dev>
2026-06-17 23:17:31 -0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        let map = new Map();
+        let freq = Array.from({length : nums.length + 1}, () => []);
+        let ans = [];
+        for (let n of nums){
+           map.set(n, (map.get(n) || 0) + 1);
+        }
+
+        for (let vals of map.keys()) {
+            console.log(vals);
+            freq[map.get(vals)].push(vals)
+        }
+
+        for (let i = freq.length - 1; i >= 0; i--) {
+            if (freq[i]) {
+                for (let vals of freq[i]) {
+                    ans.push(vals);
+                    k--;
+                    if (k === 0) return ans;
+                }
+            }
+        }
+        console.log(map);
+        console.log(freq);
+        return ans;
+    }
+}
