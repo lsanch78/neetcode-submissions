38d20e6728bfc0ff63105a36e9dfb6d4288abde5
Luis Buenrostro <hello@luisbuenrostro.dev>
2026-06-24 14:32:57 -0700

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if (nums.length === 0) return 0;
+        let set = new Set(nums);
+        let n = nums.length;
+        
+        let maxCount = 1;
+        console.log(set)
+
+        for (let i = 0; i < n; i++){
+            let val = nums[i];
+            let count = 1;
+
+            let j = i;
+
+            if (!set.has(val - 1)) {
+                while (set.has(val + 1)) {
+                    count++;
+                    val++;
+                }
+            }
+
+            if (maxCount < count) maxCount = count;
+        
+
+        }
+
+    return maxCount;
+    }
+}
