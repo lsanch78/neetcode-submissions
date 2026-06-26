bdf8f882ff46abbebb98cb3f883eb081ee2d94f9
Luis Buenrostro <hello@luisbuenrostro.dev>
2026-06-25 21:35:16 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let sanitized = s.replaceAll(/[^a-zA-Z0-9]/g,'').toLowerCase();
+        console.log(sanitized);
+
+        let i = 0;
+        let j = sanitized.length - 1;
+
+        while (i < j) {
+            if (sanitized[i] !== sanitized[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
